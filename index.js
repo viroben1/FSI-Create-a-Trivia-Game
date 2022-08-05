cf83cbe4ce468193f20fa03e1df2b1120dea0fca
benjamin farmer <viroben2001@gmail.com>
2022-08-05 12:05:08 -0700

    new repo code

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your Code Here
+let userName = window.prompt('Please enter your name:')
+let userScore = 0
+let playAgain = true
+
+function playGame() {
+    for(let i = 0; i < questions.length; i++){
+        let question = questions[i]
+        let userAnswer = window.prompt(question.text)
+        if(userAnswer === question.correctAnswer){
+            userScore = userScore + 10
+        }
+    }
+    window.alert('Your score is: '+userScore)
+}
+
+while (playAgain === true) {
+    playGame()
+    let userChoice = window.prompt('Would you like to play the game again? Answer yes or no.')
+    if(userChoice === 'yes'){
+            playAgain = true
+        } else {
+            playAgain = false
+            window.alert('Thanks for playing the game!')
+        }
+}
+
+
+
+
+
+
